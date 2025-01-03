7e52afa34205da7816709bec968ba8952d2965ea
Pallav <iampallav698@gmail.com>
2025-01-03 23:26:40 +0530

    added new lines

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const name = 'John';
 const age = 25;
-const city = 'New York';
+const city = 'New Orleans';
 
 function sayhello() {
   console.log(`Hello, my name is ${name}, I am ${age} years old and I live in ${city}`);
+}
+
+function newFunction(){
+    console.log('Hello');
 }
