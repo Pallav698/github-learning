efb382fd3706c4de531f16ee787b1ecf22df3b02
Pallav <iampallav698@gmail.com>
2025-01-04 00:09:47 +0530

    Revert "added new lines"
    This reverts commit 7e52afa34205da7816709bec968ba8952d2965ea.

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const name = 'John';
 const age = 25;
-const city = 'New Orleans';
+const city = 'New York';
 
 function sayhello() {
   console.log(`Hello, my name is ${name}, I am ${age} years old and I live in ${city}`);
-}
-
-function newFunction(){
-    console.log('Hello');
 }
